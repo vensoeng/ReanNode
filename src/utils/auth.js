74e3309cace15b672c757af45bb2c47f3f27a80b
vensoeng <likakonpouw@gmail.com>
2026-05-29 16:54:52 +0700

    update blog ux and fix item from backend

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-// export const API_URL = process.env.VITE_API_URL || 'http://localhost:5000';
-export const API_URL =  process.env.VITE_API_URL || 'https://vensoengapi.vercel.app/';
+export const API_URL = process.env.VITE_API_URL || 'https://vensoengapi.vercel.app/' || 'http://localhost:5000';
 export const ACCESS_TOKEN_KEY = "accessToken";
 export const REFRESH_TOKEN_KEY = "refreshToken";
 export const USER_KEY = "authUser";
